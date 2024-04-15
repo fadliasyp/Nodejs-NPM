65e3e0727062149b77dac973aadbaaec43dd7101
fadliasyp <riansyahfadli12@gmail.com>
2024-04-15 21:02:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import fs from 'fs/promises'
+
+// fs.writeFileSync('fadli.log', 'fadli asyp riansyah')
+fs.writeFile('fadli.log','hello nodejs')
---
+++ lodash.js
@@ -0,0 +1,5 @@
+import _ from "lodash"
+
+const name = 'FADLI ASYP RIANSYAH'
+const result = _.capitalize(name)
+console.info(result)
